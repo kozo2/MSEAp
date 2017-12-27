944e8aea6b38f6fec09cf3ffd248b79d9e5d75e2
Kozo Nishida <knishida@riken.jp>
2017-12-28 08:09:26 +0900

    test: Stop using vdiffr and check the vectors in MSEA data.frame to plot.

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -1,11 +1,18 @@
 
 context("MSEAp plots")
 
-test_that("plot msea", {
-  skip_on_cran()
+test_that("MSEA columns for plot functions", {
+  ## Does MSEA result has the vectors for the (dot)plot functions?
   res <- msea(mset_SMPDB_format_KEGG, kusano)
-  plot_msea <- function() plot(res)
-  dotplot_msea <- function() dotplot(res)
-  vdiffr::expect_doppelganger("msea plot", plot_msea)
-  vdiffr::expect_doppelganger("msea dotplot", dotplot(res))
+  expect_true("Expected" %in% names(res))
+  expect_true("Metaboliteset.name" %in% names(res))
+  expect_true("p.value" %in% names(res))
+  expect_true("Hit" %in% names(res))
+
+  ## skip_on_cran()    
+  ## plot_msea <- function() plot(res)
+  ## dotplot_msea <- function() dotplot(res)
+  ## vdiffr::expect_doppelganger("msea plot", plot_msea)
+  ## vdiffr::expect_doppelganger("msea dotplot", dotplot(res))
+
 })
