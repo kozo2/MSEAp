ef7498845f433b1e4e5aa334a97781fcc1d92e76
Kozo Nishida <knishida@riken.jp>
2018-01-08 12:39:51 +0900

    test: Add expect_equal for the shared metabolites between the metabolite sets

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -8,6 +8,11 @@ test_that("MSEA columns for plot functions", {
   expect_true("Metaboliteset.name" %in% names(res))
   expect_true("p.value" %in% names(res))
   expect_true("Hit" %in% names(res))
+  
+  foo <- netplot(res, mset_SMPDB_format_KEGG, shared.metabolite = 20)
+  expect_equal(as.character(foo$from[1]), "SMP00007")
+  expect_equal(as.character(foo$to[1]), "SMP00351")
+  expect_equal(as.character(foo$shared[1]), "C00001 C00003 C00004 C00005 C00006 C00007 C00010 C00011 C00014 C00016 C00018 C00024 C00025 C00026 C00027 C00038 C00049 C00061 C00070 C00076 C00080 C00099 C00106 C00135 C00222 C00386 C00429 C00864 C00986 C01152 C01262 C02642 C05665")
 
   ## skip_on_cran()    
   ## plot_msea <- function() plot(res)
