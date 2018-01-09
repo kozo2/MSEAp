05cddb44d1518d582cbb7dbb4a555db3738f9bf9
afukushima <afukushima@gmail.com>
2018-01-09 14:00:47 +0900

    add display function of metabolite-sets in the package

---
+++ R/util.R
@@ -0,0 +1,14 @@
+## util.R
+
+##' Display supported metabolite-sets for metabolite set enrichment analysis (MSEA).
+##' @return metabolite-set name available in MSEAp package. 
+##' @examples
+##' supported.msets()
+##' @export
+supported.msets <- function() {
+    piqr <- utils::data(package = "MSEAp")
+
+    data.files <- piqr$results[, 3]
+    mset.files <- grep("^mset_", data.files, value = TRUE)
+    return(mset.files)
+}
